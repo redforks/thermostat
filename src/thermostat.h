8a6af73baacf205cf7ee2d95e2703f299de53a73
Red Forks <redforks@gmail.com>
2016-11-13 17:13:46 +0800

    Change heater relay PIN
    PIN 13 in arduino is the on board led, it blinks a few time on startup,
    switch PIN 13 HIGH/LOW quickly, caused relay on/off a few times rapidly.

--- src/thermostat.h
+++ src/thermostat.h
@@ -5,5 +5,5 @@
 
 #define TEMPORATURE_TARGET (204)
 
-#define HEATER_PIN 13
+#define HEATER_PIN 10
 #define HEATER_ACTION_DELAY (15*60) // minimal seconds to open/close heater, prevent switch heater too frequently.
